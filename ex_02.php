66cb2455a02a3ff34d63f57669b5e26de94594a7
luisgdoliveira-008 <luis_gd_oliveira@estudante.sesisenai.org.br>
2026-07-02 08:49:46 -0300

    ex03 concluido

--- ex_02.php
+++ ex_02.php
@@ -1,12 +1,12 @@
 <?php
 
 function inverterTexto($texto) {
-    echo "Quantidade de caracteres: " . strlen ($texto); 
+    echo "Quantidade de caracteres: " . strlen ($texto) . "<br>"; 
     return strrev($texto);
 }
 $stringOriginal = "Eduardogay";
 $resultado = inverterTexto($stringOriginal);
 
-echo " Texto invertido: " . $resultado . "\n";
+echo " Texto invertido: " . $resultado ;
 
 ?>
