9b09b724c345c276b988b8d6415cf8bc9ba47ca7
luisgdoliveira-008 <luis_gd_oliveira@estudante.sesisenai.org.br>
2026-07-09 08:52:59 -0300

    ex10 concluido

---
+++ ex_10.php
@@ -0,0 +1,33 @@
+<?php
+
+function calcularMedia($notas) {
+    if (empty($notas)) {
+        return "Nenhuma nota informada.";
+    }
+
+    $maiorNota = max($notas);
+    $menorNota = min($notas);
+
+    $soma = array_sum($notas);
+    $quantidade = count($notas);
+    $media = $soma / $quantidade;
+
+    if ($media >= 7.0) {
+        $situacao = "Aprovado";
+    } elseif ($media >= 5.0) {
+        $situacao = "Recuperação";
+    } else {
+        $situacao = "Reprovado";
+    }
+
+    return [
+        'maiorNota' => $maiorNota,
+        'menorNota' => $menorNota,
+        'media' => round($media, 2), // Arredonda para 2 casas decimais
+        'situacao' => $situacao
+    ];
+}
+
+$notasAluno = [8.5, 6.0, 7.2, 5.5];
+print_r(calcularMedia($notasAluno));
+?>
